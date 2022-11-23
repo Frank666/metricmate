4b25bd201098a4269be79155f21c98a84f9e81e5
Frank <80236329+Frank666@users.noreply.github.com>
2022-11-23 17:45:56 -0600

    fixes

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -23,22 +23,22 @@ const HeadingStyle = styled(Box)({
     textAlign: "center",
 });
 
-let easing = [0.6, -0.05, 0.01, 0.99];
-const fadeInUp = {
-    initial: {
-        y: 60,
-        opacity: 0,
-        transition: { duration: 0.6, ease: easing },
-    },
-    animate: {
-        y: 0,
-        opacity: 1,
-        transition: {
-            duration: 0.6,
-            ease: easing,
-        },
-    },
-};
+//let easing = [0.6, -0.05, 0.01, 0.99];
+// const fadeInUp = {
+//     initial: {
+//         y: 60,
+//         opacity: 0,
+//         transition: { duration: 0.6, ease: easing },
+//     },
+//     animate: {
+//         y: 0,
+//         opacity: 1,
+//         transition: {
+//             duration: 0.6,
+//             ease: easing,
+//         },
+//     },
+// };
 
 function Login() {
     return (
